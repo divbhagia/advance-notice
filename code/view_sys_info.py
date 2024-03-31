e0ddf9b269609d6c912e49fdd2eecbf049e697d5
Div <dbhagia17@gmail.com>
2024-03-31 15:08:02 -0700

    edits to view_sys_info

--- code/view_sys_info.py
+++ code/view_sys_info.py
@@ -10,3 +10,14 @@ print(f"Total Memory: {vm.total / (1024 ** 3):.2f} GB")
 print(f"Available Memory: {vm.available / (1024 ** 3):.2f} GB")
 print(f"Used Memory: {vm.used / (1024 ** 3):.2f} GB")
 print(f"Memory Usage: {vm.percent}%")
+
+# Python version
+import platform
+print(f"Python version: {platform.python_version()}")
+
+# Package versions
+import numpy, pandas, matplotlib, sklearn
+print(f"NumPy version: {numpy.__version__}")
+print(f"Pandas version: {pandas.__version__}")
+print(f"Matplotlib version: {matplotlib.__version__}")
+print(f"Scikit-learn version: {sklearn.__version__}")
